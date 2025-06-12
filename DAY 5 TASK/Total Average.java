8f06a4595e805c496705300bf8d8e4ad3a17aed6
rahulrossi46 <2322ja35@kgcas.com>
2025-06-12 09:34:11 +0530

    DAY 5 TASKS

---
+++ DAY 5 TASK/Total Average.java
@@ -0,0 +1,15 @@
+class totavg{
+    
+	public static void main(String[] args)
+   {
+        int total, a=1025;
+        double average;
+        total=a+10+15+75+69+46;
+        average =total/7;
+       	 System.out.println("total:"+total);
+       	 System.out.println("average:"+average);
+
+
+    }
+
+}
